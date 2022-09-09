ea0a957e0f6195421d7e83e9f530f1a145d8f7a4
javicaib <javicaib.mail@gmail.com>
2022-09-09 23:07:52 +0200

    Configuraciones Rest

--- API/LocalAPI/LocalAPI/settings.py
+++ API/LocalAPI/LocalAPI/settings.py
@@ -30,14 +30,22 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+BASE_APPS = ['django.contrib.admin',
+             'django.contrib.auth',
+             'django.contrib.contenttypes',
+             'django.contrib.sessions',
+             'django.contrib.messages',
+             'django.contrib.staticfiles',
+             ]
+THIRDY_APPS = [
+    'rest_framework'
 ]
+
+APPS = [
+    
+]
+
+INSTALLED_APPS = BASE_APPS + THIRDY_APPS + APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -103,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
